3186fc6bade42c8f0689db12d1d1bf34bd488869
Takuya Hirata <takuyahirata4@gmail.com>
2024-12-26 13:52:09 -0500

    Fix: linting

--- src/actions/motorcycle.ts
+++ src/actions/motorcycle.ts
@@ -1,6 +1,4 @@
 'use server'
-import { redirect } from 'next/navigation'
-import { auth } from '@/auth'
 import prisma from '@/lib/prisma'
 
 export async function fetchModels(id: string) {
@@ -19,12 +17,6 @@ export async function saveMotorcycle(
   _: any,
   formData: FormData
 ) {
-  const session = await auth()
-
-  if (!session) {
-    return redirect('/')
-  }
-
   const data = {
     userId: userId,
     modelId: formData.get('modelId') as string,
