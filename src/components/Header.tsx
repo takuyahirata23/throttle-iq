a478e062cf58f3a0ea6625f64239625783133387
Takuya Hirata <takuyahirata4@gmail.com>
2024-12-27 19:34:35 -0500

    Style:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { Menu } from './Menu'
 
-export function Header() {
+export async function Header() {
   return (
     <header className="bg-primary text-primary-foreground shadow">
       <div className="mx-auto max-w-6xl w-11/12 py-4 flex justify-between items-center">
@@ -9,7 +9,7 @@ export function Header() {
           <strong className="text-lg">ThrottleIQ</strong>
           🏍️
         </Link>
-        <div>
+        <div className="flex gap-x-2">
           <Menu />
         </div>
       </div>
